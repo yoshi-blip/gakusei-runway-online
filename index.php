e2800f70e715ba368c53d894912c47c41f9d385c
yoshi-blip <yoshi@yuki-planning.co.jp>
2026-04-24 03:37:09 +0000

    test deploy

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
+  GNU nano 7.2                                                                                                        index.php                                                                                                                 
 <?php
+echo "GitHub連携成功！";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                [ Read 2 lines ]
+^G Help          ^O Write Out     ^W Where Is      ^K Cut           ^T Execute       ^C Location      M-U Undo         M-A Set Mark     M-] To Bracket   M-Q Previous     ^B Back          ^◂ Prev Word     ^A Home          ^P Prev Line
+^X Exit          ^R Read File     ^\ Replace       ^U Paste         ^J Justify       ^/ Go To Line    M-E Redo         M-6 Copy         ^Q Where Was     M-W Next         ^F Forward       ^▸ Next Word     ^E End           ^N Next <?php
 echo "Hello World";
